e734eb0224a50e61e66cc1b8800245df93cb6d62
kunal <kunal2130.be23@chitkara.edu.in>
2025-07-31 10:34:15 +0530

    String question using recirsions

--- Recursion/CharactersRemove.java
+++ Recursion/CharactersRemove.java
@@ -2,8 +2,9 @@ package Recursion;
 import java.util.Scanner;
 
 public class CharactersRemove {
-    static String Remove2
-    static String Remove(String s , int idx){
+
+         //Method 1 
+        static String Remove(String s , int idx){
         if(idx == s.length()) return "";
         // recursive work 
         String smallans = Remove(s,idx+1);
@@ -11,7 +12,16 @@ public class CharactersRemove {
         // self work 
         if(currChar != 'a') return currChar + smallans;
         else return smallans;
+    }
 
+    // Method 2 with more compateble without idx 
+     static String Remove2(String s ){
+
+        if(s.length() == 0) return "";
+        String smallAns = Remove2(s.substring(1));
+        char currChar = s.charAt(0);
+        if(currChar != 'a') return currChar + smallAns;
+        else return smallAns;
 
     }
     public static void main(String[] args) {
@@ -25,8 +35,10 @@ public class CharactersRemove {
         //  }
         //  System.out.println(s.substring(2,4)); // 2 ,3 
         String s = sc.nextLine();
-        int idx  = 0;
+        int idx = 0;
         System.out.println(Remove(s,idx));
+        System.out.println(Remove2(s));
         sc.close();
     }
 }
+ 
