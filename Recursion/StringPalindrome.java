abe6c11bf3c629198e4fd9f334f7d6bd838217e1
kunal <kunalgarg6648@gmail.com>
2025-08-04 11:40:47 +0530

    Updated code

--- Recursion/StringPalindrome.java
+++ Recursion/StringPalindrome.java
@@ -2,24 +2,45 @@ package Recursion;
 import java.util.Scanner;
 
 public class StringPalindrome {
-   static String Reverse(String  s , int idx){
-        if(idx == s.length()) return "";
+   // static String Reverse(String  s , int idx){
+   //      if(idx == s.length()) return "";
        
-        String smallAns = Reverse(s,idx+1);
+   //      String smallAns = Reverse(s,idx+1);
         
-         return smallAns + s.charAt(idx);
+   //       return smallAns + s.charAt(idx);
        
 
+   //  }
+   //optimized Method 
+
+    public static boolean check(String str, int s ,int e){
+      if(s>e){
+         return true;
+      }
+         if(str.charAt(s)!=str.charAt(e)){
+            return false;
+         }
+         return check(str,s+1,e-1);
     }
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.print("Enter the Character: ");
        String s = sc.nextLine();
-       int idx = 0;
-       String rev = Reverse(s,idx);
-       if(rev.equals(s)) System.out.println("  The String is Palindrome");
-       else System.out.println(" The String is not Palindrome");
-       sc.close();
+       
+      //  String rev = Reverse(s,0);
+      //  if(rev.equals(s)) System.out.println("  The String is Palindrome");
+      //  else System.out.println(" The String is not Palindrome");
+        sc.close();
+
+        if(check(s,0,s.length()-1)){
+         System.out.println("The String is Palindrome");
+        }else{
+         System.out.println("The String is not Palindrome");
+
+        }
+
+
+
     }
        
     
