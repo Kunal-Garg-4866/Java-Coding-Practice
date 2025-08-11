b2f3aad8cff79500a73a76911d387a80c27a5d3b
kunal <kunalgarg6648@gmail.com>
2025-08-11 10:23:05 +0530

    More optimized code

--- SORTING/BubbleSort/BasicSorting.java
+++ SORTING/BubbleSort/BasicSorting.java
@@ -2,18 +2,36 @@ package SORTING.BubbleSort;
 import java.util.Scanner;
 
 public class BasicSorting {
+    // static void bubblesort(int [] arr){
+    //     int n = arr.length;
+    //      for(int i=0;i< n-1;i++ ){
+    //         for(int j=0;j<n-i-1;j++){
+
+    //             if(arr[j] > arr[j+1]){
+    //                 int temp = arr[j];
+    //                 arr[j] = arr[j+1];
+    //                 arr[j+1] = temp;
+    //             }
+
+    //         }
+    //     }
+
+    // optimized method 
     static void bubblesort(int [] arr){
         int n = arr.length;
          for(int i=0;i< n-1;i++ ){
+            boolean flag = false; // has any swap happened 
             for(int j=0;j<n-i-1;j++){
 
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = true; // 
                 }
 
             }
+            if(flag == false) return;
         }
     }
     public static void main(String[] args) {
