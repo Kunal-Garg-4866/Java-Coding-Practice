6d01e9c8fbffec3f6cd8b577387907156a781a7a
kunal <kunalgarg6648@gmail.com>
2025-08-13 12:23:50 +0530

    more optimized move Zeroes Solution

--- SORTING/Practice/MovesZeroes.java
+++ SORTING/Practice/MovesZeroes.java
@@ -6,14 +6,17 @@ public class MovesZeroes {
     static void Zeroes(int [] arr){
         int n = arr.length;
         for(int i=0;i<n-1;i++){
+            boolean flag = false;
             for(int j=0;j<n-i-1;j++){
                 if(arr[j]== 0 && arr[j+1] !=0){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag = true;
                 }
                 
             }
+            if(flag == false) return;
         }
     }
     public static void main(String[] args) {
