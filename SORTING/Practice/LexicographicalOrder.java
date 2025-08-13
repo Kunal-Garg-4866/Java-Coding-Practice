8514238f9a716153d31167c6b8d2588773573c28
kunal <kunalgarg6648@gmail.com>
2025-08-13 13:11:50 +0530

    Lexicographocal order Question

---
+++ SORTING/Practice/LexicographicalOrder.java
@@ -0,0 +1,34 @@
+package SORTING.Practice;
+import java.util.Scanner;
+
+public class LexicographicalOrder {
+    static void sortFruits(String[] fruits){
+        int n = fruits.length;
+        for(int i=0;i<n-1;i++){
+            int min_index = i;
+            for(int j =i+1;j<n;j++){
+                if(fruits[j].compareTo(fruits[min_index])< 0){
+                    min_index = j;
+                }
+            }
+            String temp = fruits[i];
+            fruits[i] = fruits[min_index];
+            fruits[min_index] = temp;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] fruits = new String[n];
+        for(int i = 0; i < n; i++) {
+            fruits[i] = sc.next();
+            }
+            sortFruits(fruits);
+            for(String s : fruits) {
+                System.out.println(s);
+                }
+        sc.close();
+    }
+    
+}
