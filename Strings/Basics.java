15b335e0618360561fbc5f245d8e8eb34dfa1983
kunal <kunalgarg6648@gmail.com>
2025-10-13 10:54:14 +0530

    Today's work

--- Strings/Basics.java
+++ Strings/Basics.java
@@ -6,17 +6,26 @@ import java.util.*;
 public class Basics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = "Kunal";
-        System.out.println(str);
-        // String s  = sc.next();
-        // System.out.println(s);
-        String p = sc.nextLine();
+        // String str = "Kunal";
+        // System.out.println(str);
+        // // String s  = sc.next();
+        // // System.out.println(s);
+        // String p = sc.nextLine();
 
-         System.out.println(p);
-         char ch = str.charAt(3);
-         System.out.println(ch);
+        //  System.out.println(p);
+        //  char ch = str.charAt(3);
+        //  System.out.println(ch);
+            // String str = "physucs";
+            // System.out.println(str.contains("suc"));
+            // System.out.println(str.endsWith("s"));
+            //  System.out.println(str.startsWith("s"));
+        String s1 = "abc";
+        String s2 = "def";
+        String s3= (s1.concat(s2));
+         System.out.println(s3);
+
 
         sc.close();
-
+ 
     }
 }
