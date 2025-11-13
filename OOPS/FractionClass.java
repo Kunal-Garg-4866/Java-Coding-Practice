7c6571b0939f5ecb562b888ee4d23cd5527866fd
kunal <kunalgarg6648@gmail.com>
2025-11-13 10:03:56 +0530

    Fraction Class

---
+++ OOPS/FractionClass.java
@@ -0,0 +1,45 @@
+package OOPS;
+
+public class FractionClass {
+    public static int gcd (int num, int dem){
+
+        int min = Math.min(num,dem);
+        for(int i=min;i>=1;i--){
+            if(num%i==0 && dem%i==0) return i;
+
+        }
+        return min;
+    }
+    public static class Fraction{
+        int num;
+        int dem;
+    public Fraction(int num , int dem){
+        this.num = num;
+        this.dem = dem;
+    }
+     public  void simplify(){
+        // this method will simplify the fraction but not in all cases 
+    //     if(num<dem){
+    //         if(dem%num==0){
+    //             dem = dem/num;
+    //             num = 1;
+    //         }
+    //     }
+
+    // now this method 
+        int hcf = gcd(num,dem);
+        num/= hcf;
+        dem/= hcf;
+    }
+
+}
+    public static void main(String[] args) {
+         Fraction f1 = new Fraction(35,21);
+         f1.simplify();
+         System.out.println(f1.num+"/"+f1.dem);
+
+        
+    }
+    
+}
+
--- OOPS/StudendClass.java
+++ OOPS/StudendClass.java
@@ -22,12 +22,12 @@ public class StudendClass {
 //         System.out.println(s1.name);
 //         System.out.println(s1.getrno());
 //         System.out.println(s1.percentage);
-            Student s1 = new Student ("Bob", 45, 92.3);
-            System.out.println(Student.numberofStudent); 
-            Student s2 = new Student ("Bob", 45, 92.3);
-            System.out.println(s2.numberofStudent);
-            Student s3 = new Student ("Bob", 45, 92.3);
-            System.out.println(s3.numberofStudent);
+            // Student s1 = new Student ("Bob", 45, 92.3);
+            // System.out.println(Student.numberofStudent); 
+            // Student s2 = new Student ("Bob", 45, 92.3);
+            // System.out.println(Student.numberofStudent);
+            // Student s3 = new Student ("Bob", 45, 92.3);
+            // System.out.println(Student.numberofStudent);
 
       
     }   
